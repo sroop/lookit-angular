aa45f3640763480321d7f1fa838b46d539082733
Sroop Sunar <sroopsunar@hotmail.com>
2015-02-20 12:00:02 +0000

    Create a form where user can add a title and post.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -10,4 +10,9 @@ angular.module('lookit', ['templates'])
                       {title: "post 4", upvotes: 9},
                       {title: "post 5", upvotes: 5}
                     ];
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title == "") { return; }
+      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.title = "";
+    };
   }]);

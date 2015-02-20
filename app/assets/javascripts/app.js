6c55cd7f914226d20b8a485334166bd84773e427
Sroop Sunar <sroopsunar@hotmail.com>
2015-02-20 12:40:57 +0000

    Create a ‘posts’ service, move the posts array into the service, inject into the controller and bind to scope variable to access it.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,15 +1,17 @@
 angular.module('lookit', ['templates'])
+.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
+
 .controller('MainCtrl', [
   '$scope',
-  function($scope){
+  'posts',
+  function($scope, posts){
+    $scope.posts = posts.posts;
     $scope.test = "Hello world!";
-    $scope.posts =  [
-                      {title: "post 1", upvotes: 2},
-                      {title: "post 2", upvotes: 4},
-                      {title: "post 3", upvotes: 15},
-                      {title: "post 4", upvotes: 9},
-                      {title: "post 5", upvotes: 5}
-                    ];
     $scope.addPost = function(){
       if(!$scope.title || $scope.title == "") { return; }
       $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});

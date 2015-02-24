b79c39f567a28c4d7b99215e107b5bf0d62c8549
Sroop Sunar <sroopsunar@hotmail.com>
2015-02-24 12:29:13 +0000

    Add author attribute to comments model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220170327) do
+ActiveRecord::Schema.define(version: 20150224122602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150220170327) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "author"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree

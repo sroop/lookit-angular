2d4d7296be1dad7713838feefff073cebd5351be
Sroop Sunar <sroopsunar@hotmail.com>
2015-02-24 16:24:28 +0000

    Create login and register states + templates.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -25,6 +25,18 @@ function($stateProvider, $urlRouterProvider) {
         }]
       },
       controller: 'PostsCtrl'
+    })
+
+    .state('login', {
+      url: '/login',
+      templateUrl: 'auth/_login.html',
+      controller: 'AuthCtrl'
+    })
+
+    .state('register', {
+      url: '/register',
+      templateUrl: 'auth/_register.html',
+      controller: 'AuthCtrl'
     });
 
   $urlRouterProvider.otherwise('home');
---
+++ app/assets/javascripts/auth/authCtrl.js
@@ -0,0 +1,22 @@
+angular.module('lookit')
+.controller('AuthCtrl', [
+  '$scope',
+  '$state',
+  'Auth',
+  function($scope, $state, Auth){
+
+    $scope.login = function() {
+      Auth.login($scope.user).then(function(){
+        $state.go('home');
+      });
+    };
+
+    $scope.register = function() {
+      Auth.register($scope.user).then(function(){
+        $state.go('home');
+      });
+    };
+
+  }
+
+])

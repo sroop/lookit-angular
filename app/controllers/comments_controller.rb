c6b29e3b4f5bff4351ead967f368e72eff093c53
Sroop Sunar <sroopsunar@hotmail.com>
2015-02-24 12:30:14 +0000

    Oops - fix #upvote action in comments controller to find a post.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,10 +7,11 @@ class CommentsController < ApplicationController
   end
 
   def upvote
+    post = Post.find(params[:post_id])
     comment = post.comments.find(params[:id])
     comment.increment!(:upvotes)
 
-    respond_with post,comment
+    respond_with post, comment
   end
 
   private

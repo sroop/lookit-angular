f0f431b921740b2c94bd58e7505b26acf9be9490
Sroop Sunar <sroopsunar@hotmail.com>
2015-02-25 12:09:00 +0000

    Oops - Fix nav bar and navCtrl to correctly allow logging in/logging out with angular-devise.

--- app/assets/javascripts/nav/navCtrl.js
+++ app/assets/javascripts/nav/navCtrl.js
@@ -2,21 +2,37 @@ angular.module('lookit')
 .controller('NavCtrl', [
   '$scope',
   'Auth',
-  function($scope, Auth) {
+  '$state',
+  function($scope, Auth, $state) {
 
-    $scope.signedIn = Auth.isAuthenticated();
-    $scope.logout = Auth.logout();
+    $scope.logout = function() {
+      Auth.logout($scope.user).then(function(){
+        console.log("Bye!");
+        $state.go('home');
+      });
+    };
 
-    $scope.$on('devise:new-registration', function (e, user){
-        $scope.user = user;
-    });
-
-    $scope.$on('devise:login', function (e, user){
+    Auth.currentUser().then(function (user){
+      console.log(user);
+      $scope.signedIn = Auth.isAuthenticated();
       $scope.user = user;
     });
 
-    $scope.$on('devise:logout', function (e, user){
-      $scope.user = {};
+    $scope.$on('devise:new-registration', function (e, user) {
+      console.log("New registration");
+      $scope.signedIn = Auth.isAuthenticated();
+      $scope.user = user;
+    });
+
+    $scope.$on('devise:login', function (e, user) {
+      console.log("Login");
+      $scope.signedIn = Auth.isAuthenticated();
+      $scope.user = user;
+    });
+
+    $scope.$on('devise:logout', function (e, user) {
+      console.log(user.username + " is logging out...");
+      $scope.signedIn = Auth.isAuthenticated();
     });
 
   }
